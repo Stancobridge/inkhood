2244f7e7e1e93f9835cdb6b7ef082be674ee9e40
stancobridge <soromgawide@gmail.com>
2020-04-02 23:56:55 +0000

    drop menu animation: removed timer

--- timer.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * x is count that can have any number from 0 to infinity 
- * m is x converted in minutes // x = 60m hence m = 1m
- * h is x converted to hour   // x = 7200s hence h = 2hrs
- * s: when x is between 0-60, s will be the same value of x, but at 61 s will return back to 0;
- * and start counting from there until x gets to 120,
- * s will return to 0 again, this will keep on repeating like music player count
- */
